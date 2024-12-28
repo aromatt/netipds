f4f745353e55b97177a6595194987f2036903c6e
Andrew Matteson <amatteson3@gmail.com>
2024-12-28 16:28:59 -0500

    Reduce branching in uint128.isBitSet

--- key.go
+++ key.go
@@ -58,11 +58,11 @@ func (k key) toPrefix() netip.Prefix {
 // bit is used as a selector for a node's children.
 //
 // bitL refers to the left child, and bitR to the right.
-type bit = bool
+type bit = uint8
 
 const (
-	bitL = false
-	bitR = true
+	bitL = 0
+	bitR = 1
 )
 
 var eachBit = [2]bit{bitL, bitR}
