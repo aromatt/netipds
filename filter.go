91f7b91aa1fa9de588e3d760c77c265e350e19e5
Andrew Matteson <amatteson3@gmail.com>
2025-03-17 13:22:39 -0400

    Add filter

---
+++ filter.go
@@ -0,0 +1,51 @@
+package netipds
+
+// filter is a simple Bloom-like filter for uint128 keys.
+type filter struct {
+	// ones is simply all keys ORed together.
+	ones uint128
+	// zeros is the OR of all keys' inverses. It contains a 1 in every position
+	// where no key has a 1.
+	zeros uint128
+	// minLen is the minimum length of keys that have been inserted.
+	minLen uint8
+	// maxLen is the maximum length of keys that have been inserted.
+	maxLen uint8
+}
+
+// insert adds k to the filter.
+func (f filter) insert(k key[uint128]) filter {
+	f.ones = f.ones.or(k.content)
+	f.zeros = f.zeros.or(k.content.not())
+	if f.minLen == 0 || k.len < f.minLen {
+		f.minLen = k.len
+	}
+	if k.len > f.maxLen {
+		f.maxLen = k.len
+	}
+	return f
+}
+
+// mightContain returns true if the filter might contain k.
+func (f filter) mightContain(k key[uint128]) bool {
+	if k.len < f.minLen || k.len > f.maxLen {
+		return false
+	}
+	if f.ones.and(k.content) != k.content {
+		return false
+	}
+	notk := k.content.not()
+	if f.zeros.and(notk) != notk {
+		return false
+	}
+	return true
+}
+
+// mightContainPrefix returns true if the filter might contain a key that is a
+// prefix of k.
+func (f filter) mightContainPrefix(k key[uint128]) bool {
+	com1 := f.ones.and(k.content).commonPrefixLen(k.content)
+	notk := k.content.not()
+	com0 := f.zeros.and(notk).commonPrefixLen(notk)
+	return com1 >= f.minLen && com0 >= f.minLen
+}
---
+++ filter_test.go
@@ -0,0 +1,75 @@
+package netipds
+
+import (
+	"testing"
+)
+
+func TestFilterMightContain(t *testing.T) {
+	tests := []struct {
+		insert []key[keyBits6]
+		query  key[keyBits6]
+		want   bool
+	}{
+		{[]key[keyBits6]{k6(uint128{0, 1}, 0, 128)}, k6(uint128{0, 1}, 0, 128), true},
+		{[]key[keyBits6]{k6(uint128{0, 1}, 0, 128)}, k6(uint128{0, 0}, 0, 128), false},
+
+		{[]key[keyBits6]{k6(uint128{0, 2}, 0, 128)}, k6(uint128{0, 2}, 0, 128), true},
+		{[]key[keyBits6]{k6(uint128{0, 1}, 0, 128)}, k6(uint128{0, 2}, 0, 128), false},
+
+		{[]key[keyBits6]{k6(uint128{0, 3}, 0, 128)}, k6(uint128{0, 3}, 0, 128), true},
+		{[]key[keyBits6]{k6(uint128{0, 3}, 0, 128)}, k6(uint128{0, 1}, 0, 128), false},
+
+		{
+			[]key[keyBits6]{
+				k6(uint128{0, 2}, 0, 128),
+				k6(uint128{0, 1}, 0, 128),
+			},
+			k6(uint128{0, 1}, 0, 128),
+			true,
+		},
+
+		{
+			[]key[keyBits6]{
+				k6(uint128{0, 2}, 0, 128),
+				k6(uint128{0, 1}, 0, 128),
+			},
+			k6(uint128{0, 3}, 0, 128),
+			true,
+		},
+
+		{[]key[keyBits6]{k6(uint128{0, 2}, 0, 127)}, k6(uint128{0, 2}, 0, 127), true},
+		{[]key[keyBits6]{k6(uint128{0, 2}, 0, 127)}, k6(uint128{0, 2}, 0, 128), false},
+	}
+	for _, tt := range tests {
+		f := filter{}
+		for _, k := range tt.insert {
+			f = f.insert(k)
+		}
+		if got := f.mightContain(tt.query); got != tt.want {
+			t.Errorf("f.mightContain(%v) = %v, want %v", tt.query, got, tt.want)
+		}
+	}
+}
+
+func TestFilterMightContainPrefix(t *testing.T) {
+	tests := []struct {
+		insert []key[keyBits6]
+		query  key[keyBits6]
+		want   bool
+	}{
+		{[]key[keyBits6]{k6(uint128{0, 1}, 0, 128)}, k6(uint128{0, 1}, 0, 128), true},
+		{[]key[keyBits6]{k6(uint128{0, 1}, 0, 128)}, k6(uint128{0, 0}, 0, 128), false},
+
+		{[]key[keyBits6]{k6(uint128{0, 0}, 0, 127)}, k6(uint128{0, 1}, 0, 128), true},
+		{[]key[keyBits6]{k6(uint128{0, 2}, 0, 127)}, k6(uint128{0, 3}, 0, 128), true},
+	}
+	for _, tt := range tests {
+		f := filter{}
+		for _, k := range tt.insert {
+			f = f.insert(k)
+		}
+		if got := f.mightContainPrefix(tt.query); got != tt.want {
+			t.Errorf("f.mightContainPrefix(%v) = %v, want %v", tt.query, got, tt.want)
+		}
+	}
+}
