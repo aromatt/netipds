09e2bc66133badebaebf0d59ddc7608191aad653
Andrew Matteson <amatteson3@gmail.com>
2025-04-25 15:13:07 -0400

    Uncomment PrefixSet iterator methods

--- prefixset_iter_test.go
+++ prefixset_iter_test.go
@@ -2,7 +2,6 @@
 
 package netipds
 
-/* HACK
 import (
 	"iter"
 	"net/netip"
@@ -10,7 +9,7 @@ import (
 	"testing"
 )
 
-func TestPrefixSetAll(t *testing.T) {
+func TestPrefixSetAll6(t *testing.T) {
 	tests := []struct {
 		add  []netip.Prefix
 		want []netip.Prefix
@@ -25,7 +24,6 @@ func TestPrefixSetAll(t *testing.T) {
 		{pfxs("::0/1", "8000::/1"), pfxs("::0/1", "8000::/1")},
 		{pfxs("0::0/127", "::0/128", "::1/128"), pfxs("0::0/127", "::0/128", "::1/128")},
 		{pfxs("0::0/127", "::0/128", "::2/128"), pfxs("0::0/127", "::0/128", "::2/128")},
-		{pfxs("1.2.3.0/24", "1.2.3.4/32"), pfxs("1.2.3.0/24", "1.2.3.4/32")},
 	}
 	for _, tt := range tests {
 		psb := &PrefixSetBuilder{}
@@ -33,41 +31,189 @@ func TestPrefixSetAll(t *testing.T) {
 			psb.Add(p)
 		}
 		ps := psb.PrefixSet()
-		seq := ps.All()
-		checkPrefixSeq(t, seq, tt.want)
-		checkYieldFalse(t, seq)
+
+		seqAll6 := ps.All6()
+		checkPrefixSeq(t, seqAll6, tt.want)
+		checkYieldFalse(t, seqAll6)
+
+		// All should yield the same items since only IPv6s were added
+		seqAll := ps.All()
+		checkPrefixSeq(t, seqAll, tt.want)
+		checkYieldFalse(t, seqAll)
 	}
 }
 
-func TestPrefixSetAllCompact(t *testing.T) {
+func TestPrefixSetAll4(t *testing.T) {
 	tests := []struct {
 		add  []netip.Prefix
 		want []netip.Prefix
 	}{
 		{pfxs(), pfxs()},
-		{pfxs("::0/128"), pfxs("::0/128")},
-		{pfxs("::0/128", "::1/128"), pfxs("::0/128", "::1/128")},
-		{pfxs("::0/127", "::0/128"), pfxs("::0/127")},
-		{pfxs("::0/126", "::0/127"), pfxs("::0/126")},
-		{pfxs("::0/1", "::0/128"), pfxs("::0/1")},
-		{pfxs("8000::/1"), pfxs("8000::/1")},
-		{pfxs("::0/1", "8000::/1"), pfxs("::0/1", "8000::/1")},
-		{pfxs("0::0/127", "::0/128", "::1/128"), pfxs("::0/127")},
-		{pfxs("0::0/127", "::0/128", "::2/128"), pfxs("::0/127", "::2/128")},
-		{pfxs("1.2.3.0/24", "1.2.3.4/32"), pfxs("1.2.3.0/24")},
+		{pfxs("1.2.3.4/32"), pfxs("1.2.3.4/32")},
+		{pfxs("1.2.3.4/32", "1.2.3.5/32"), pfxs("1.2.3.4/32", "1.2.3.5/32")},
+		{pfxs("1.2.3.4/31", "1.2.3.4/32"), pfxs("1.2.3.4/31", "1.2.3.4/32")},
+		{pfxs("1.2.3.4/30", "1.2.3.4/31"), pfxs("1.2.3.4/30", "1.2.3.4/31")},
+		{pfxs("0.0.0.0/1", "1.2.3.4/32"), pfxs("0.0.0.0/1", "1.2.3.4/32")},
+		{pfxs("128.0.0.0/1"), pfxs("128.0.0.0/1")},
+		{pfxs("0.0.0.0/1", "128.0.0.0/1"), pfxs("0.0.0.0/1", "128.0.0.0/1")},
+		{
+			pfxs("1.2.3.4/31", "1.2.3.4/32", "1.2.3.5/32"),
+			pfxs("1.2.3.4/31", "1.2.3.4/32", "1.2.3.5/32"),
+		},
+		{
+			pfxs("1.2.3.4/31", "1.2.3.4/32", "1.2.3.6/32"),
+			pfxs("1.2.3.4/31", "1.2.3.4/32", "1.2.3.6/32"),
+		},
 	}
+
 	for _, tt := range tests {
 		psb := &PrefixSetBuilder{}
 		for _, p := range tt.add {
 			psb.Add(p)
 		}
 		ps := psb.PrefixSet()
-		seq := ps.AllCompact()
-		checkPrefixSeq(t, seq, tt.want)
-		checkYieldFalse(t, seq)
+
+		seqAll4 := ps.All4()
+		checkPrefixSeq(t, seqAll4, tt.want)
+		checkYieldFalse(t, seqAll4)
 	}
 }
 
+func TestPrefixSetAll(t *testing.T) {
+	tests := []struct {
+		add4 []netip.Prefix
+		add6 []netip.Prefix
+		want []netip.Prefix
+	}{
+		// no IPv4, no IPv6
+		{nil, nil, nil},
+
+		// IPv4 only
+		{
+			pfxs("1.2.3.4/32", "128.0.0.0/1"),
+			nil,
+			pfxs("1.2.3.4/32", "128.0.0.0/1"),
+		},
+
+		// IPv6 only
+		{
+			nil,
+			pfxs("::0/128", "::1/128"),
+			pfxs("::0/128", "::1/128"),
+		},
+
+		// mixed families
+		{
+			pfxs("1.2.3.4/32", "128.0.0.0/1"),
+			pfxs("::0/128", "8000::/1"),
+			append(
+				pfxs("1.2.3.4/32", "128.0.0.0/1"),
+				pfxs("::0/128", "8000::/1")...,
+			),
+		},
+	}
+
+	for _, tt := range tests {
+		psb := &PrefixSetBuilder{}
+		for _, p := range tt.add4 {
+			psb.Add(p)
+		}
+		for _, p := range tt.add6 {
+			psb.Add(p)
+		}
+		ps := psb.PrefixSet()
+
+		seqAll := ps.All()
+		checkPrefixSeq(t, seqAll, tt.want)
+		checkYieldFalse(t, seqAll)
+	}
+}
+
+func TestPrefixSetAllCompact4(t *testing.T) {
+	tests := []struct {
+		add  []netip.Prefix
+		want []netip.Prefix
+	}{
+		{pfxs(), pfxs()},
+		{pfxs("1.2.3.4/32"), pfxs("1.2.3.4/32")},
+		{pfxs("1.2.3.4/32", "1.2.3.5/32"), pfxs("1.2.3.4/32", "1.2.3.5/32")},
+		{pfxs("1.2.3.4/31", "1.2.3.4/32"), pfxs("1.2.3.4/31")},
+		{pfxs("1.2.3.4/30", "1.2.3.4/31"), pfxs("1.2.3.4/30")},
+		{pfxs("0.0.0.0/1", "1.2.3.4/32"), pfxs("0.0.0.0/1")},
+		{pfxs("128.0.0.0/1"), pfxs("128.0.0.0/1")},
+		{pfxs("0.0.0.0/1", "128.0.0.0/1"), pfxs("0.0.0.0/1", "128.0.0.0/1")},
+		{
+			pfxs("1.2.3.4/31", "1.2.3.4/32", "1.2.3.5/32"),
+			pfxs("1.2.3.4/31"),
+		},
+		{
+			pfxs("1.2.3.4/31", "1.2.3.4/32", "1.2.3.6/32"),
+			pfxs("1.2.3.4/31", "1.2.3.6/32"),
+		},
+	}
+
+	for _, tt := range tests {
+		psb := &PrefixSetBuilder{}
+		for _, p := range tt.add {
+			psb.Add(p)
+		}
+		ps := psb.PrefixSet()
+
+		seq4 := ps.AllCompact4()
+		checkPrefixSeq(t, seq4, tt.want)
+		checkYieldFalse(t, seq4)
+	}
+}
+
+func TestPrefixSetAllCompact(t *testing.T) {
+	tests := []struct {
+		add4 []netip.Prefix
+		add6 []netip.Prefix
+		want []netip.Prefix
+	}{
+		// no IPv4, no IPv6
+		{nil, nil, nil},
+
+		// IPv4 only
+		{
+			pfxs("1.2.3.4/31", "1.2.3.4/32"),
+			nil,
+			pfxs("1.2.3.4/31"),
+		},
+
+		// IPv6 only
+		{
+			nil,
+			pfxs("::0/127", "::0/128", "::2/128"),
+			pfxs("::0/127", "::2/128"),
+		},
+
+		// mixed families
+		{
+			pfxs("1.2.3.4/31", "1.2.3.4/32"),
+			pfxs("::0/127", "::0/128", "::2/128"),
+			append(
+				pfxs("1.2.3.4/31"),
+				pfxs("::0/127", "::2/128")...,
+			),
+		},
+	}
+
+	for _, tt := range tests {
+		psb := &PrefixSetBuilder{}
+		for _, p := range tt.add4 {
+			psb.Add(p)
+		}
+		for _, p := range tt.add6 {
+			psb.Add(p)
+		}
+		ps := psb.PrefixSet()
+
+		seqAll := ps.AllCompact()
+		checkPrefixSeq(t, seqAll, tt.want)
+		checkYieldFalse(t, seqAll)
+	}
+}
 func checkPrefixSeq(t *testing.T, seq iter.Seq[netip.Prefix], want []netip.Prefix) {
 	t.Helper()
 	got := slices.AppendSeq(make([]netip.Prefix, 0, len(want)), seq)
@@ -87,4 +233,3 @@ func checkYieldFalse[T any](t *testing.T, seq iter.Seq[T]) {
 		t.Fatal("iteration continued after yield returned false")
 	}
 }
-*/
